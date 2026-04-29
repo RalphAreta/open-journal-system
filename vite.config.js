29a9bdb0c0dccc0a42c428bd8fe1a6226a2be306
Carlos Ruben <22-08941@g.batstate-u.edu.ph>
2026-04-29 14:45:58 +0800

    fixed bugged

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,6 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: '172.21.121.151', // ← palitan ng totoong IP mo (ipconfig)
-        },
 
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
     },
 });
